0eaa3b9d0f4e0f67b5c9cfd67455238f217aa204
SirDimas <dimassixer@gmail.com>
2016-08-03 10:13:44 +0700

    Master Product and Supplier

---
+++ test/index.js
@@ -0,0 +1,12 @@
+function test(name, path) {
+    describe(name, function () {
+        require(path);
+    })
+}
+
+describe('#dl-module', function (done) {
+    this.timeout(2 * 60000); 
+    //test('@manager/product-manager', './managers/product-manager-test'); 
+    test('@manager/supplier-manager', './managers/supplier-manager-test');
+
+})
