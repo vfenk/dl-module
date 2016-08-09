c1ddd4fc223a0932a219e43d122d6f07709a83fa
indriHutabalian <indri.hutabalian@moonlay.com>
2016-08-09 17:53:52 +0700

    add Master Textile (#3)
    * add Master Textile
    
    * update test index

--- test/index.js
+++ test/index.js
@@ -6,8 +6,9 @@ function test(name, path) {
 
 describe('#dl-module', function (done) {
     this.timeout(2 * 60000);
-   // test('@manager/product-manager', './managers/product-manager-test');
-   // test('@manager/supplier-manager', './managers/supplier-manager-test');
-  //  test('@manager/buyer-manager', './managers/buyer-manager');
+    test('@manager/product-manager', './managers/product-manager-test');
+    test('@manager/supplier-manager', './managers/supplier-manager-test');
+    test('@manager/buyer-manager', './managers/buyer-manager');
     test('@manager/accessories-manager', './managers/accessories-manager');
+    test('@manager/textile-manager', './managers/textile-manager-test');
 })
