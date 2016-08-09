17176bdf0f9545850396f497154e4b253640464c
Hotline Mei <Hotline.Tambunan@moonlay.jakarta>
2016-08-09 17:03:15 +0700

    Accessories Module

---
+++ src/managers/core/accessories-manager.js
@@ -0,0 +1,302 @@
+'use strict'
+
+// external deps 
+var ObjectId = require("mongodb").ObjectId;
+
+// internal deps
+require('mongodb-toolkit');
+var DLModels = require('dl-models');
+var map = DLModels.map;
+var Accessories = DLModels.core.Accessories;
+
+module.exports = class AccessoriesManager{
+    constructor(db, user){
+        this.db = db;
+        this.user = user;
+        this.accessoriesCollection = this.db.use(map.core.Accessories);
+    }
+
+    read(paging){
+        var _paging = Object.assign({
+            page: 1,
+            size: 20,
+            order: '_id',
+            asc: true
+        }, paging);
+
+        return new Promise((resolve, reject)=>{
+            var deleted = {
+                _deleted : false
+            };
+            var query = _paging.keyword ? {
+                '$and': [deleted]
+            } : deleted;
+
+            if (_paging.keyword) {
+                var regex = new RegExp(_paging.keyword, "i");
+                var filterCode = {
+                    'code': {
+                        '$regex': regex
+                    }
+                };
+                var filterName = {
+                    'name': {
+                        '$regex': regex
+                    }
+                };
+                var $or = {
+                    '$or': [filterCode, filterName]
+                };
+
+                query['$and'].push($or);
+            }
+
+
+            this.accessoriesCollection
+                .where(query)
+                .page(_paging.page, _paging.size)
+                .orderBy(_paging.order, _paging.asc)
+                .execute()
+                .then(accessories => {
+                    resolve(accessories);
+                })
+                .catch(e => {
+                    reject(e);
+            });
+        });
+    }
+
+    readByAccessoriesId(accessoriesId, paging) {
+        var _paging = Object.assign({
+            page: 1,
+            size: 20,
+            order: '_id',
+            asc: true
+        }, paging);
+
+        return new Promise((resolve, reject) => {
+            var deleted = {
+                _deleted: false
+            };
+            var accessories = {
+                accessoriesId: new ObjectId(accessoriesId)
+            };
+            var query = {
+                '$and': [deleted, module]
+            };
+
+            if (_paging.keyword) {
+                var regex = new RegExp(_paging.keyword, "i");
+                var filterCode = {
+                    'code': {
+                        '$regex': regex
+                    }
+                };
+                var filterName = {
+                    'name': {
+                        '$regex': regex
+                    }
+                };
+                var $or = {
+                    '$or': [filterCode, filterName]
+                };
+
+                query['$and'].push($or);
+            }
+
+
+            this.accessoriesCollection
+                .where(query)
+                .page(_paging.page, _paging.size)
+                .orderBy(_paging.order, _paging.asc)
+                .execute()
+                .then(accessories => {
+                    resolve(accessories);
+                })
+                .catch(e => {
+                    reject(e);
+                });
+        });
+    }
+
+   getById(id) {
+        return new Promise((resolve, reject) => {
+            if (id === '')
+                resolve(null);
+            var query = {
+                _id: new ObjectId(id),
+                _deleted: false
+            };
+            this.getSingleByQuery(query)
+                .then(module => {
+                    resolve(module);
+                })
+                .catch(e => {
+                    reject(e);
+                });
+        });
+    }
+
+    getByCode(code) {
+        return new Promise((resolve, reject) => {
+            if (code === '')
+                resolve(null);
+            var query = {
+                code: code,
+                _deleted: false
+            };
+            this.getSingleByQuery(query)
+                .then(module => {
+                    resolve(module);
+                })
+                .catch(e => {
+                    reject(e);
+                });
+        });
+    }
+
+    getByIdOrDefault(id) {
+        return new Promise((resolve, reject) => {
+            if (id === '')
+                resolve(null);
+            var query = {
+                _id: new ObjectId(id),
+                _deleted: false
+            };
+            this.getSingleOrDefaultByQuery(query)
+                .then(module => {
+                    resolve(module);
+                })
+                .catch(e => {
+                    reject(e);
+                });
+        });
+    }
+
+    getSingleByQuery(query) {
+        return new Promise((resolve, reject) => {
+            this.accessoriesCollection
+                .single(query)
+                .then(module => {
+                    resolve(module);
+                })
+                .catch(e => {
+                    reject(e);
+                });
+        })
+    }
+
+     getSingleOrDefaultByQuery(query) {
+        return new Promise((resolve, reject) => {
+            this.accessoriesCollection
+                .singleOrDefault(query)
+                .then(accessories => {
+                    resolve(accessories);
+                })
+                .catch(e => {
+                    reject(e);
+                });
+        })
+    }
+
+     create(accessories) {
+        return new Promise((resolve, reject) => {
+            this._validate(accessories)
+                .then(validAccessories => {
+                    this.accessoriesCollection.insert(validAccessories)
+                        .then(id => {
+                            resolve(id);
+                        })
+                        .catch(e => {
+                            reject(e);
+                        })
+                })
+                .catch(e => {
+                    reject(e);
+                })
+        });
+    }
+
+    update(accessories) {
+        return new Promise((resolve, reject) => {
+            this._validate(accessories)
+                .then(validAccessories => {
+                    this.accessoriesCollection.update(validAccessories)
+                        .then(id => {
+                            resolve(id);
+                        })
+                        .catch(e => {
+                            reject(e);
+                        });
+                })
+                .catch(e => {
+                    reject(e);
+                });
+        });
+    }
+
+    delete(accessories) {
+        return new Promise((resolve, reject) => {
+            this._validate(accessories)
+                .then(validAccessories => {
+                    validAccessories._deleted = true;
+                    this.accessoriesCollection.update(validAccessories)
+                        .then(id => {
+                            resolve(id);
+                        })
+                        .catch(e => {
+                            reject(e);
+                        });
+                })
+                .catch(e => {
+                    reject(e);
+                });
+        });
+    }
+
+ _validate(accessories) {
+        var errors = {};
+        return new Promise((resolve, reject) => {
+            var valid = new Accessories(accessories);
+           
+            // 1. begin: Declare promises.
+            var getAccessoriesPromise = this.accessoriesCollection.singleOrDefault({
+                "$and": [{
+                    _id: {
+                        '$ne': new ObjectId(valid._id)
+                    }
+                }, {
+                        code: valid.code
+                    }]
+            });
+            // 1. end: Declare promises.
+
+            // 2. begin: Validation.
+            Promise.all([getAccessoriesPromise])
+                   .then(results => {
+                    var _module = results[0];
+
+                    if (!valid.code || valid.code == '')
+                        errors["code"] = "code is required";
+                    else if (_module) {
+                        errors["code"] = "code already exists";
+                    }
+
+                    if (!valid.name || valid.name == '')
+                        errors["name"] = "name is required"; 
+
+                    // 2c. begin: check if data has any error, reject if it has.
+                    for (var prop in errors) {
+                        var ValidationError = require('../../validation-error');
+                        reject(new ValidationError('data does not pass validation', errors));
+                    }
+
+                    valid.stamp(this.user.username, 'manager');
+                    resolve(valid);
+                })
+                .catch(e => {
+                    reject(e);
+                })
+        });
+    }
+};
