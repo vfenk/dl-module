be4990222b8c34de794fdd18380b283dcc0d937f
tr4nquility <tr4nquility_s@yahoo.com>
2016-08-10 10:23:47 +0700

    Add map for Buyer and Supplier

--- src/map.js
+++ src/map.js
@@ -1,7 +1,9 @@
 module.exports = {
     core: {
         Fabric: 'fabrics',
-        Textile: 'textiles'
-        Accessories: 'accessories'
+        Textile: 'textiles',
+        Accessories: 'accessories',
+        Buyer: 'buyers',
+        Supplier: 'suppliers'
     }
 }
