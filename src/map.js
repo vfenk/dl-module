57268dfa1a5256a5896ccd10ca6e68dcecc9ef5e
tr4nquility <tr4nquility_s@yahoo.com>
2016-08-10 10:22:34 +0700

    Fix conflict

--- src/map.js
+++ src/map.js
@@ -1,11 +1,7 @@
 module.exports = {
     core: {
         Fabric: 'fabrics',
-<<<<<<< HEAD
         Textile: 'textiles'
-=======
-        Textile: 'textiles',
         Accessories: 'accessories'
->>>>>>> b4f55d97f499289cf9ca76e86193451824b74d8a
     }
 }
