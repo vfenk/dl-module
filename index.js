e9b2523e2a27fe424e7433c5cd0c8416b4f6ff2c
tr4nquility <tr4nquility_s@yahoo.com>
2016-08-10 13:36:53 +0700

    Master (#13)

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ module.exports = {
         Supplier: require('./src/core/supplier'),
         Textile: require('./src/core/textile'),
         Accessories: require('./src/core/accessories'),
-        UoM: require('./src/core/UoM-docs')
+        UoM: require('./src/core/UoM-docs').UoM,
+        UoM_Template: require('./src/core/UoM-docs').UoM_Template
     },
     map: require('./src/map'),
     validator: require('./test/validator')
