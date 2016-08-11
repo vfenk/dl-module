9f943203384c4a0ea53c83d68469a1cfa686f051
tr4nquility <tr4nquility_s@yahoo.com>
2016-08-11 11:55:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,16 +7,10 @@ module.exports = {
         Supplier: require('./src/core/supplier'),
         Textile: require('./src/core/textile'),
         Accessories: require('./src/core/accessories'),
-<<<<<<< HEAD
         UoM: require('./src/core/UoM').UoM,
         UoM_Template: require('./src/core/UoM').UoM_Template
-=======
         Sparepart: require('./src/core/sparepart'),
-        UoM: require('./src/core/UoM-docs').UoM,
-        UoM_Template: require('./src/core/UoM-docs').UoM_Template
->>>>>>> 0497f73247decc757c586fc057001d5a47833177
     },
     map: require('./src/map'),
     validator: require('./test/validator')
-
 }
