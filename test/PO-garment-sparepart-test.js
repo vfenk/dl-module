3d8cd428dd035ea649821385d69ec11a3c787a7c
tr4nquility <tr4nquility_s@yahoo.com>
2016-08-12 17:30:37 +0700

    Update PO-garment-sparepart-test.js

--- test/PO-garment-sparepart-test.js
+++ test/PO-garment-sparepart-test.js
@@ -1,53 +1,4 @@
 var validate = require('./validator').po;
-// it("#06. PO Garment SparePart should valid", function () {
-//     var POGarmentSparepart = require('../src/po/PO-garment-sparepart');
-//     var Supplier = require('../src/po/supplier');
-//     var SparepartValue = require('../src/po/sparepart-value');
-//     var Sparepart = require('../src/po/sparepart');
-//     var UoM_Template = require('../src/po/UoM-docs').UoM_Template;
-//     var UoM = require('../src/po/UoM-docs').UoM;
-
-//     var supplier = new Supplier(
-//         code = '123',
-//         name = 'hot',
-//         description = 'hotline',
-//         phone = '0812....',
-//         address = 'test',
-//         local = true
-//     );
-//     var template = new UoM_Template({
-//         mainUnit: 'M',
-//         mainValue: 1,
-//         convertedUnit: 'M',
-//         convertedValue: 1 
-//     });
-
-//     var _units = [];
-//     _units.push(template);
-//     var _uom = new UoM({
-//         category: 'UoM-Unit-Test',
-//         default: template,
-//         units: _units
-//     });
-
-//     var sparepart = new Sparepart({
-//         code = '22',
-//         name = 'hotline',
-//         description = 'hotline123',
-//         uom =_uom
-//     });
-
-//     var sparepartValue = new SparepartValue();
-//     sparepartValue.sparepart = sparepart;
-
-//     var pOGarmentSparepart = new POGarmentSparepart();
-
-//     pOGarmentSparepart.supplier = supplier;
-//     pOGarmentSparepart.sparepartValue = sparepartValue;
-
-//     validate.pOGarmentSparepart(pOGarmentSparepart);
-// })
-
 it("#06. PO Garment SparePart should valid", function () {
     var POGarmentSparepart = require('../src/po/PO-garment-sparepart');
     var Supplier = require('../src/core/supplier');
@@ -113,8 +64,3 @@ it("#06. PO Garment SparePart should valid", function () {
     validate.POGarmentSparePart(pOGarmentSparepart);
 
 })
-
-// it("#06. PO Garment SparePart should valid", function () {
-//     var POGarmentSparePart = require('../src/po/PO-garment-sparepart');
-//     validate.POGarmentSparePart(new POGarmentSparePart());
-// })
