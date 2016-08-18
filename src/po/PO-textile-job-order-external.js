22df785fcdb55c9743e410df8a996373ff68c581
indriHutabalian <indri.hutabalian@moonlay.com>
2016-08-18 15:32:52 +0700

    update map (#41)
    * update path of PO textile
    
    * change map of product

--- src/po/PO-textile-job-order-external.js
+++ src/po/PO-textile-job-order-external.js
@@ -1,6 +1,6 @@
 'use strict'
 var PurchaseOrder = require('./purchase-order');
-var map = require('dl-models').map.po;
+var map =  require('../map').po;
 module.exports = class POTextileJobOrder extends PurchaseOrder {
     constructor(source) {
         super(source, map.type.POTextileJobOrderExternal);
--- test/index.js
+++ test/index.js
@@ -9,7 +9,5 @@ test('@buyer', './buyer');
 test('@supplier', './supplier');
 test('@product', './product');
 test('@uom', './UoM');
-// test('@POGarmentSparepart', './PO-garment-sparepart-test');
-// test('@POTextileJobOrder','./PO-textile-joborder-test');
 test('@PurchaseOrder', './purchase-order-test');
 test('@PurchaseOrderGroup', './purchase-order-group-test');
