2e2b5398a87624bcd3a3636ed981163262d42921
kanisiusandrew <kanisius.andrew@moonlay.com>
2016-08-24 10:43:26 +0700

    modify poitem model

--- src/po/purchase-order-item.js
+++ src/po/purchase-order-item.js
@@ -3,9 +3,9 @@ var BaseModel = require('capital-models').BaseModel;
 var Product = require('../core/product');
 
 module.exports = class PurchaseOrderItem extends BaseModel {
-    constructor(source) {
+    constructor(source, type) {
         super(type || 'purchase-order-item', '1.0.0');
-        
+
         this.qty = 0;
         this.price = 0;
         this.product = new Product();
