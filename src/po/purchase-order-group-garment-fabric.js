28db51b3e6c352bfb235ec48ae8f2dee9b6a4134
kanisiusandrew <kanisius.andrew@moonlay.com>
2016-08-24 15:48:22 +0700

    remove pogroupgarment models

--- src/po/purchase-order-group-garment-fabric.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-var PurchaseOrderGroup = require('./purchase-order-group');
-var map = require('../map');
-module.exports = class POGroupGarmentFabric extends PurchaseOrderGroup {
-    constructor(source) {
-        super(source, map.po.type.POGarmentFabric);
-    }
-}
--- src/po/purchase-order-group-garment-sparepart.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-var PurchaseOrderGroup = require('./purchase-order-group');
-var map = require('../map');
-module.exports = class POGroupGarmentSparepart extends PurchaseOrderGroup {
-    constructor(source) {
-        super(source, map.po.type.POGarmentSparepart);
-    }
-}
