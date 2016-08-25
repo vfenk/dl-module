877d9f9f27c07adc4d5d9b003892ff630b9eef8b
kanisiusandrew <kanisius.andrew@moonlay.com>
2016-08-25 16:09:33 +0700

    add po textile sparepart

---
+++ src/po/purchase-order-textile-sparepart.js
@@ -0,0 +1,9 @@
+'use strict'
+var PurchaseOrder = require('./purchase-order');
+var map = require('../map');
+module.exports = class POTextileSparepart extends PurchaseOrder {
+    constructor(source) {
+        super(source, map.po.type.POTextileSparepart);
+        this.iso = 'FM-600-06-005';
+    }
+}
