f7ed778fb69ee5852ad40f2ed288b9e96160caf7
JanuitaM <januita.marpaung@moonlay.com>
2016-11-08 16:44:43 +0700

    add variabel tasks and getPurchaseOrderById in unit-payment-price-correction-note-manager

--- src/managers/purchasing/unit-payment-price-correction-note-manager.js
+++ src/managers/purchasing/unit-payment-price-correction-note-manager.js
@@ -112,8 +112,10 @@ module.exports = class UnitPaymentPriceCorrectionNoteManager extends BaseManager
                                 if (_purchaseOrderId.equals(_unitReceiptNoteItem.purchaseOrder._id) && _productId.equals(_unitReceiptNoteItem.product._id)) {
                                     item.purchaseOrderId = new ObjectId(_unitReceiptNoteItem.purchaseOrder._id);
                                     item.purchaseOrder = _unitReceiptNoteItem.purchaseOrder;
+                                    item.purchaseOrder._id= new ObjectId(_unitReceiptNoteItem.purchaseOrder._id);
                                     item.productId = new ObjectId(_unitReceiptNoteItem.product._id);
                                     item.product = _unitReceiptNoteItem.product;
+                                    item.product._id= new ObjectId(_unitReceiptNoteItem.product._id);
                                     item.quantity = _unitReceiptNoteItem.deliveredQuantity;
                                     item.uom = _unitReceiptNoteItem.deliveredUom;
                                     item.uomId = new ObjectId(_unitReceiptNoteItem.deliveredUom._id);
@@ -326,6 +328,8 @@ module.exports = class UnitPaymentPriceCorrectionNoteManager extends BaseManager
                 .then((createIndexResults) => {
                     this._validate(unitPaymentPriceCorrectionNote)
                         .then(validData => {
+                            var getPurchaseOrderById = [];
+                            var tasks = [];
                             //Update PO Internal
                             var poId = new ObjectId();
                             for (var _item of validData.items) {
@@ -393,6 +397,8 @@ module.exports = class UnitPaymentPriceCorrectionNoteManager extends BaseManager
                 .then((createIndexResults) => {
                     this._validate(unitPaymentPriceCorrectionNote)
                         .then(validData => {
+                            var tasks = [];
+                            var getPurchaseOrderById = [];
                             validData._deleted = true;
                             //Update PO Internal
                             var poId = new ObjectId();
@@ -420,7 +426,7 @@ module.exports = class UnitPaymentPriceCorrectionNoteManager extends BaseManager
                                                             break;
                                                         }
                                                     }
-                                                    _unitPaymentPriceCorrectionNoteItem.purchaseOrder = _purchaseOrder;
+                                                    _unitPaymentPriceCorrectionNoteItem.purchaseOrder = _purchaseOrder; 
                                                     _unitPaymentPriceCorrectionNoteItem.purchaseOrderId = new ObjectId(_purchaseOrder._id);
 
                                                 }
