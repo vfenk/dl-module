064f9a6f16f197c9642111d31bbf1faffd7350a4
SirDimas <dimassixer@gmail.com>
2016-11-14 10:28:31 +0700

    machine module

--- src/managers/master/machine-manager.js
+++ src/managers/master/machine-manager.js
@@ -9,21 +9,21 @@ var Machine = DLModels.master.Machine;
 var BaseManager = require('../base-manager');
 var i18n = require('dl-i18n');
 var CodeGenerator = require('../../utils/code-generator');
+var UnitManager = require('./unit-manager');
 
 module.exports = class MachineManager extends BaseManager {
     constructor(db, user) {
         super(db, user);
         this.collection = this.db.collection(map.master.collection.Machine);
+        this.unitManager = new UnitManager(db, user);
     }
 
     _getQuery(paging) {
-        var deleted = {
+        var deletedFilter = {
             _deleted: false
-        };
-        var query = paging.keyword ? {
-            '$and': [deleted]
-        } : deleted;
+        }, keywordFilter = {};
 
+        var query = {};
         if (paging.keyword) {
             var regex = new RegExp(paging.keyword, "i");
             var filterCode = {
@@ -36,12 +36,12 @@ module.exports = class MachineManager extends BaseManager {
                     '$regex': regex
                 }
             };
-            var $or = {
+            keywordFilter = {
                 '$or': [filterCode, filterName]
             };
-
-            query['$and'].push($or);
         }
+
+        query = {'$and' : [deletedFilter, paging.filter, keywordFilter]};
         return query;
     }
 
@@ -52,22 +52,21 @@ module.exports = class MachineManager extends BaseManager {
             // 1. begin: Declare promises.
             var getMachinePromise = this.collection.singleOrDefault({
                 "$and": [{
-                    "$and": [{
                         _id: {
                             '$ne': new ObjectId(valid._id)
                         }
                     }, {
-                            name: valid.name
-                        }]
-                },
-                {
-                    _deleted:false
-                } ]
+                        name: valid.name
+                    }, {
+                        _deleted:false
+                }]
             });
+            var getUnit = valid.unit && valid.unit._id ? this.unitManager.getSingleByIdOrDefault(valid.unit._id) : Promise.resolve(null);
             // 2. begin: Validation.
-            Promise.all([getMachinePromise])
+            Promise.all([getMachinePromise, getUnit])
                 .then(results => {
                     var _machine = results[0];
+                    var _unit = results[1];
                     valid.code = CodeGenerator();
                     
                     if (!valid.name || valid.name == '')
@@ -75,6 +74,8 @@ module.exports = class MachineManager extends BaseManager {
                     else if (_machine) {
                         errors["name"] = i18n.__("Machine.name.isExists:%s is required", i18n.__("Machine.name._:Node")); //"Kode sudah ada";
                     }
+                    if(!_unit)
+                        errors["unit"] = i18n.__("Machine.unit.isExists:%s is not exists", i18n.__("Machine.unit._:Node")); //"Unit tidak ada";
 
                     // 2c. begin: check if data has any error, reject if it has.
                      if (Object.getOwnPropertyNames(errors).length > 0) {
@@ -82,7 +83,13 @@ module.exports = class MachineManager extends BaseManager {
                         reject(new ValidationError('data does not pass validation', errors));
                     }
 
-                    valid = new Machine(machine);
+                    if(_unit){
+                        valid.unit = _unit;
+                        valid.unitId = new ObjectId(_unit._id);
+                    }
+
+                    if(!valid.stamp)
+                        valid = new Machine(valid);
                     valid.stamp(this.user.username, 'manager');
                     resolve(valid);
                 })
