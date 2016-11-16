13335d81d12d9e5ac346851020d6b863345a5725
SirDimas <dimassixer@gmail.com>
2016-11-16 09:18:10 +0700

    fixed machine module (#188)
    * fixed machine module
    
    * Add Module
    
    add module Uster Classification and modul winding quality sampling
    
    * Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ module.exports = {
             CurrencyManager: require('./src/managers/master/currency-manager'),
             VatManager: require('./src/managers/master/vat-manager'),
             BudgetManager: require('./src/managers/master/budget-manager'),
-            MachineManager: require('./src/managers/master/machine-manager')
+            MachineManager: require('./src/managers/master/machine-manager'),
+            UsterClassification: require('./src/managers/master/uster-classification-manager')
         },
         purchasing: {
             PurchaseOrderManager: require('./src/managers/purchasing/purchase-order-manager'),
@@ -25,6 +26,13 @@ module.exports = {
             PurchaseRequestManager: require('./src/managers/purchasing/purchase-request-manager'),
             UnitPaymentPriceCorrectionNoteManager: require('./src/managers/purchasing/unit-payment-price-correction-note-manager'),
             UnitPaymentOrderManager: require('./src/managers/purchasing/unit-payment-order-manager')
+        },
+        production : {
+            spinning : {
+                winding : {
+                    WindingQualitySampling : require('./src/managers/production/spinning/winding/winding-quality-sampling-manager')
+                }
+            }
         }
     }
 }
