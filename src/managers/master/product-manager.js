1e0d58075458eed6dc37a9c8c462cbb116f34bb7
achensetyawati <achenzetya@gmail.com>
2016-11-23 14:24:41 +0700

    fixing product manager

--- src/managers/master/product-manager.js
+++ src/managers/master/product-manager.js
@@ -46,24 +46,7 @@ module.exports = class ProductManager extends BaseManager {
 
             query['$and'].push($or);
         }
-        if (paging.tags)
-        {
-            var regex = new RegExp(paging.tags, "i");
-            var filterName = {
-                'name': {
-                    '$regex': regex
-                }
-            };
-            var filterTags = {
-                'tags': {
-                    '$regex': regex
-                }
-            };
-            var $or = {
-                '$or': [filterTags, filterName]
-            };
-            query['$and'].push($or);
-        }
+        
         return query;
     }
 
