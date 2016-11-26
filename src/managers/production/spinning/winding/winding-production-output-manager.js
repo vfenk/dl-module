c37c43ab3f10abee18ef81d7efe8399cfc0a44d2
achensetyawati <achenzetya@gmail.com>
2016-11-26 11:27:16 +0700

    fixing validation winding output

--- src/managers/production/spinning/winding/winding-production-output-manager.js
+++ src/managers/production/spinning/winding/winding-production-output-manager.js
@@ -118,28 +118,37 @@ module.exports = class WindingProductionOutputManager extends BaseManager {
                 var _Ts=null;
                 var now = new Date();
                 valid.product=_product;
-
-                if(_lotmachine.data.length > 0){
-                    for(var a of _lotmachine.data)
-                    {
-                        if(a.productId==valid.productId && a.machineId==valid.machineId) {
-                        // if(a._id.toString() == valid.lotMachineId.toString()) {
-                            _Lm = a;
-                            break;
+                valid.unitId=new ObjectId(_unit._id);
+                valid.machineId=new ObjectId(_machine._id);
+                valid.productId=new ObjectId(_product._id);
+                if(_lotmachine)
+                {
+                    if(_lotmachine.data.length > 0){
+                        for(var a of _lotmachine.data)
+                        {
+                            if(a.productId==valid.productId && a.machineId==valid.machineId) {
+                            // if(a._id.toString() == valid.lotMachineId.toString()) {
+                                _Lm = a;
+                                break;
+                            }
                         }
                     }
                 }
-
-                if(_threadSpecification.data.length > 0){
-                    for(var b of _threadSpecification.data)
-                    {
-                        if(b.productId==valid.productId) {
-                        // if(b._id.toString() == valid.threadSpecificationId.toString()) {
-                            _Ts = b;
-                            break;
+                
+                if(_threadSpecification)
+                {
+                    if(_threadSpecification.data.length > 0){
+                        for(var b of _threadSpecification.data)
+                        {
+                            if(b.productId==valid.productId) {
+                            // if(b._id.toString() == valid.threadSpecificationId.toString()) {
+                                _Ts = b;
+                                break;
+                            }
                         }
                     }
                 }
+                
                 
                 if(_module){
                     errors["shift"] = i18n.__(`WindingProductionOutput.shift.isRequired:%s with same Product, Machine, Spinning and Date is already exists`, i18n.__("WindingQualitySampling.shift._:Shift")); //"Spinning dengan produk, mesin dan tanggal,shift,dan mesin yang sama tidak boleh";
@@ -212,21 +221,19 @@ module.exports = class WindingProductionOutputManager extends BaseManager {
                 }
 
                 if (!_Ts)
-                    errors["threadSpecification"] = i18n.__("WindingProductionOutput.threadSpecification.isRequired:%s is not exists", i18n.__("WindingProductionOutput.threadSpecification._:ThreadSpecification")); 
+                    errors["product"] = i18n.__("WindingProductionOutput.threadSpecification.isRequired:%s is not exists", i18n.__("WindingProductionOutput.threadSpecification._:ThreadSpecification")); 
                 else if (!valid.threadSpecificationId)
-                    errors["threadSpecification"] = i18n.__("WindingProductionOutput.threadSpecification.isRequired:%s is required", i18n.__("WindingProductionOutput.threadSpecification._:ThreadSpecification"));
+                    errors["product"] = i18n.__("WindingProductionOutput.threadSpecification.isRequired:%s is required", i18n.__("WindingProductionOutput.threadSpecification._:ThreadSpecification"));
                 else if (valid.threadSpecification) {
                     if (!valid.threadSpecification._id)
-                        errors["threadSpecification"] = i18n.__("WindingProductionOutput.threadSpecification.isRequired:%s is required", i18n.__("WindingProductionOutput.threadSpecification._:ThreadSpecification"));
+                        errors["product"] = i18n.__("WindingProductionOutput.threadSpecification.isRequired:%s is required", i18n.__("WindingProductionOutput.threadSpecification._:ThreadSpecification"));
                 }
 
                 if (Object.getOwnPropertyNames(errors).length > 0) {
                     var ValidationError = require('../../../../validation-error');
                     reject(new ValidationError('data does not pass validation', errors));
                 }
-                valid.unitId=new ObjectId(_unit._id);
-                valid.machineId=new ObjectId(_machine._id);
-                valid.productId=new ObjectId(_product._id);
+                
 
                 valid = new WindingProductionOutput(windingProductionOutput);
                 valid.stamp(this.user.username, 'manager');
