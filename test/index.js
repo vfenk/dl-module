74ce99112ddae085e32c2f215a9a4cda775fe82e
achensetyawati <achenzetya@gmail.com>
2016-12-08 09:11:23 +0700

    adding unit test report

--- test/index.js
+++ test/index.js
@@ -9,31 +9,31 @@ describe('#dl-module', function(done) {
     this.timeout(2 * 60000);
 
     // Auth
-    test('@auth/account-manager', './auth/account-manager-test');
-    test('@auth/role-manager', './auth/role-manager-test');
+    // test('@auth/account-manager', './auth/account-manager-test');
+    // test('@auth/role-manager', './auth/role-manager-test');
 
     // test('@MASTER/BUDGET', './etl/fact-purchase-duration');
     // // Master
-    test('@MASTER/BUDGET', './master/budget');
-    test('@MASTER/BUYER', './master/buyer');
-    test('@MASTER/CATEGORY', './master/category');
-    test('@MASTER/CURRENCY', './master/currency');
-    test('@MASTER/DIVISION', './master/division');
-    test('@MASTER/LOT-MACHINE', './master/lot-machine');
-    test('@MASTER/MACHINE', './master/machine');
-    test('@MASTER/PRODUCT', './master/product');
-    test('@MASTER/SUPPLIER', './master/supplier');
-    test('@MASTER/THREAD-SPECIFICATION', './master/thread-specification');
-    test('@MASTER/UOM', './master/uom');
-    test('@MASTER/UNIT', './master/unit');
-    test('@MASTER/USTER', './master/uster');
-    test('@MASTER/VAT', './master/vat');
-    test('@MASTER/YARN-EQUIVALENT-CONVERSION', './master/yarn-equivalent-coversion');
-    test('@MASTER/LAMP-STANDARD', './master/lamp-standard');
-    test('@MASTER/ACCOUNT-BANK', './master/account-bank');
+    // test('@MASTER/BUDGET', './master/budget');
+    // test('@MASTER/BUYER', './master/buyer');
+    // test('@MASTER/CATEGORY', './master/category');
+    // test('@MASTER/CURRENCY', './master/currency');
+    // test('@MASTER/DIVISION', './master/division');
+    // test('@MASTER/LOT-MACHINE', './master/lot-machine');
+    // test('@MASTER/MACHINE', './master/machine');
+    // test('@MASTER/PRODUCT', './master/product');
+    // test('@MASTER/SUPPLIER', './master/supplier');
+    // test('@MASTER/THREAD-SPECIFICATION', './master/thread-specification');
+    // test('@MASTER/UOM', './master/uom');
+    // test('@MASTER/UNIT', './master/unit');
+    // test('@MASTER/USTER', './master/uster');
+    // test('@MASTER/VAT', './master/vat');
+    // test('@MASTER/YARN-EQUIVALENT-CONVERSION', './master/yarn-equivalent-coversion');
+    // test('@MASTER/LAMP-STANDARD', './master/lamp-standard');
+    // test('@MASTER/ACCOUNT-BANK', './master/account-bank');
 
-    // //Purchasing 
-     test('@PURCHASING/PURCHASE REQUEST', './purchasing/purchase-request');
+    // // //Purchasing 
+    //  test('@PURCHASING/PURCHASE REQUEST', './purchasing/purchase-request');
     // test('@purchasing/purchase-request-manager', './purchasing/purchase-request-manager-test');
     // test('@purchasing/purchase-order-manager', './purchasing/purchase-order-manager-test');
     // test('@purchasing/purchase-order-external-manager', './purchasing/purchase-order-external-manager-test');
@@ -57,8 +57,8 @@ describe('#dl-module', function(done) {
 
     // //Production
 
-    test('@production/winding-quality-sampling-manager', './production/spinning/winding/winding-quality-sampling-manager-test');
-    test('@production/winding-production-output-manager', './production/spinning/winding/winding-production-output-manager-test');
+    // test('@production/winding-quality-sampling-manager', './production/spinning/winding/winding-quality-sampling-manager-test');
+    // test('@production/winding-production-output-manager', './production/spinning/winding/winding-production-output-manager-test');
 
 
 })
