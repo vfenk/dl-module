2339f54ab689c34c9fec525ddea55e4acc16e69a
Leslie Aula <cristiano.ulo@gmail.com>
2016-12-21 18:40:27 +0700

    test

--- test/index.js
+++ test/index.js
@@ -12,42 +12,43 @@ describe('#dl-module', function(done) {
     // test('@auth/account-manager', './auth/account-manager-test');
     // test('@auth/role-manager', './auth/role-manager-test');
 
-    // test('@ETL/DIM-CATEGORY', './etl/dim-category');
-    // test('@ETL/DIM-DIVISION', './etl/dim-division');
-    // test('@ETL/DIM-SUPPLIER', './etl/dim-supplier');
-    // test('@ETL/DIM-UNIT', './etl/dim-unit');
-    // test('@ETL/FACT-PURCHASE-DURATION', './etl/fact-purchase-duration');
-    // test('@ETL/FACT-TOTAL-HUTANG', './etl/fact-total-hutang');
-    // test('@ETL/FACT-TOTAL-NILAI-PEMBELIAN', './etl/fact-total-nilai-pembelian');
-    // test('@ETL/FACT-TOP-TEN-SUPPLIER', './etl/fact-top-ten-supplier');
-    // test('@ETL/FACT-PURCHASE-ORDER-COMPARISON', './etl/fact-purchase-order-comparison');
+    test('@ETL/DIM-CATEGORY', './etl/dim-category');
+    test('@ETL/DIM-DIVISION', './etl/dim-division');
+    test('@ETL/DIM-SUPPLIER', './etl/dim-supplier');
+    test('@ETL/DIM-UNIT', './etl/dim-unit');
+    test('@ETL/FACT-PURCHASE-DURATION', './etl/fact-purchase-duration');
+    test('@ETL/FACT-TOTAL-HUTANG', './etl/fact-total-hutang');
+    test('@ETL/FACT-TOTAL-NILAI-PEMBELIAN', './etl/fact-total-nilai-pembelian');
+    test('@ETL/FACT-TOP-TEN-SUPPLIER', './etl/fact-top-ten-supplier');
+    test('@ETL/FACT-PURCHASE-ORDER-COMPARISON', './etl/fact-purchase-order-comparison');
     test('@ETL/FACT-TIMELINESS-SUPPLIER', './etl/fact-timeliness-supplier');
 
     // // Master
-    // test('@MASTER/ACCOUNT-BANK', './master/account-bank');
-    // test('@MASTER/BUDGET', './master/budget');
-    // test('@MASTER/BUYER', './master/buyer');
-    // test('@MASTER/CATEGORY', './master/category');
-    // test('@MASTER/CURRENCY', './master/currency');
-    // test('@MASTER/DIVISION', './master/division');
-    // test('@MASTER/LAMP-STANDARD', './master/lamp-standard');
-    // test('@MASTER/LOT-MACHINE', './master/lot-machine');
-    // test('@MASTER/MACHINE', './master/machine');
-    // test('@MASTER/PRODUCT', './master/product');
-    // test('@MASTER/SUPPLIER', './master/supplier');
-    // test('@MASTER/THREAD-SPECIFICATION', './master/thread-specification');
-    // test('@MASTER/UNIT', './master/unit');
-    // test('@MASTER/UOM', './master/uom');
-    // test('@MASTER/USTER', './master/uster');
-    // test('@MASTER/VAT', './master/vat');
-    // test('@MASTER/YARN-EQUIVALENT-CONVERSION', './master/yarn-equivalent-coversion');
+    test('@MASTER/ACCOUNT-BANK', './master/account-bank');
+    test('@MASTER/BUDGET', './master/budget');
+    test('@MASTER/BUYER', './master/buyer');
+    test('@MASTER/CATEGORY', './master/category');
+    test('@MASTER/CURRENCY', './master/currency');
+    test('@MASTER/DIVISION', './master/division');
+    test('@MASTER/LAMP-STANDARD', './master/lamp-standard');
+    test('@MASTER/LOT-MACHINE', './master/lot-machine');
+    test('@MASTER/MACHINE', './master/machine');
+    test('@MASTER/PRODUCT', './master/product');
+    test('@MASTER/SUPPLIER', './master/supplier');
+    test('@MASTER/THREAD-SPECIFICATION', './master/thread-specification');
+    test('@MASTER/UNIT', './master/unit');
+    test('@MASTER/UOM', './master/uom');
+    test('@MASTER/USTER', './master/uster');
+    test('@MASTER/VAT', './master/vat');
+    test('@MASTER/YARN-EQUIVALENT-CONVERSION', './master/yarn-equivalent-coversion');
 
     // // //Purchasing 
-    // test('@PURCHASING/PURCHASE REQUEST', './purchasing/purchase-request');
-    // test('@PURCHASING/PURCHASE ORDER', './purchasing/purchase-order');
-    // test('@PURCHASING/PURCHASE ORDER EXTERNAL', './purchasing/purchase-order-external'); 
-    // test('@PURCHASING/DELIVERY ORDER', './purchasing/delivery-order');
+    test('@PURCHASING/PURCHASE REQUEST', './purchasing/purchase-request');
+    test('@PURCHASING/PURCHASE ORDER', './purchasing/purchase-order');
+    test('@PURCHASING/PURCHASE ORDER EXTERNAL', './purchasing/purchase-order-external'); 
+    test('@PURCHASING/DELIVERY ORDER', './purchasing/delivery-order');
     
+    //  test('@purchasing/purchase-order/report', './purchasing/purchase-order/report/report');		
 
     // ok
     // test('@purchasing/delivery-order-manager', './purchasing/delivery-order-manager-test');
