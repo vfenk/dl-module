dec2a4b758c694fd3bfbdc4a1b9fe37a4104f92d
indriHutabalian <indri.hutabalian@moonlay.com>
2016-12-23 11:46:29 +0700

    fix unit test create

--- test/purchasing/unit-payment-quantity-correction-note/create.js
+++ test/purchasing/unit-payment-quantity-correction-note/create.js
@@ -2,13 +2,13 @@ require("should");
 var helper = require("../../helper");
 
 var unitPaymentQuantityCorrectionNoteDataUtil = require("../../data-util/purchasing/unit-payment-quantity-correction-note-data-util");
-var unitPaymentQuantityCorrectionNoteManager = require("../../../src/managers/purchasing/unit-payment-quantity-correction-note-manager");
+var UnitPaymentQuantityCorrectionNoteManager = require("../../../src/managers/purchasing/unit-payment-quantity-correction-note-manager");
 var unitPaymentQuantityCorrectionNoteManager = null;
 
 before('#00. connect db', function (done) {
     helper.getDb()
         .then((db) => {
-            unitPaymentQuantityCorrectionNoteManager = new unitPaymentQuantityCorrectionNoteManager(db, {
+            unitPaymentQuantityCorrectionNoteManager = new UnitPaymentQuantityCorrectionNoteManager(db, {
                 username: 'unit-test'
             });
             done();
