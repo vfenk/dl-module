eb796ff9e618ac4897dc56c7186a2363d886304d
indriHutabalian <indri.hutabalian@moonlay.com>
2016-12-23 10:43:31 +0700

    add unit test of quanity correction

--- test/purchasing/unit-payment-quantity-correction-note/create.js
+++ test/purchasing/unit-payment-quantity-correction-note/create.js
@@ -1,14 +1,14 @@
 require("should");
 var helper = require("../../helper");
 
-var unitPaymentQuantityCorrectionNote = require('../../data').transaction.unitPaymentQuantityCorrectionNote;
-var UnitPaymentQuantityCorrectionNoteManager = require("../../../src/managers/purchasing/unit-payment-quantity-correction-note-manager");
+var unitPaymentQuantityCorrectionNoteDataUtil = require("../../data-util/purchasing/unit-payment-quantity-correction-note-data-util");
+var unitPaymentQuantityCorrectionNoteManager = require("../../../src/managers/purchasing/unit-payment-quantity-correction-note-manager");
 var unitPaymentQuantityCorrectionNoteManager = null;
 
 before('#00. connect db', function (done) {
     helper.getDb()
         .then((db) => {
-            unitPaymentQuantityCorrectionNoteManager = new UnitPaymentQuantityCorrectionNoteManager(db, {
+            unitPaymentQuantityCorrectionNoteManager = new unitPaymentQuantityCorrectionNoteManager(db, {
                 username: 'unit-test'
             });
             done();
@@ -34,7 +34,7 @@ it('#01. should error when create with empty data ', function(done) {
 });
 
 it('#02. should success when create new unit-payment-quantity-correction-note', function (done) {
-    unitPaymentQuantityCorrectionNote.getNew()
+    unitPaymentQuantityCorrectionNoteDataUtil.getNewTestData()
         .then((data) => {
             data._id.should.be.Object();
             createdId = data._id;
@@ -42,5 +42,21 @@ it('#02. should success when create new unit-payment-quantity-correction-note', 
         })
         .catch(e => {
             done(e);
+        });
+});
+
+
+it('#03. should success when get pdf ', function(done) {
+    unitPaymentQuantityCorrectionNoteManager.pdf(createdId)
+        .then((binary) => {
+            done();
+        })
+        .catch(e => {
+            try {
+                done();
+            }
+            catch (ex) {
+                done(ex);
+            }
         });
 });
