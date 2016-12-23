2c321926b1f99087bf263dd14ccdba25cc6599ab
indriHutabalian <indri.hutabalian@moonlay.com>
2016-12-23 10:50:42 +0700

    fix unit test

--- test/purchasing/unit-payment-price-correction-note/basic.js
+++ test/purchasing/unit-payment-price-correction-note/basic.js
@@ -1,5 +1,5 @@
 var options = {
-    manager: require("../../../src/managers/purchasing/unit-payment-price-correction-manager"),
+    manager: require("../../../src/managers/purchasing/unit-payment-price-correction-note-manager"),
     model: require("dl-models").purchasing.UnitPaymentCorrectionNote,
     util: require("../../data-util/purchasing/unit-payment-price-correction-data-util"),
     validator: require("dl-models").validator.purchasing.unitPaymentCorrectionNote,
--- test/purchasing/unit-payment-quantity-correction-note/basic.js
+++ test/purchasing/unit-payment-quantity-correction-note/basic.js
@@ -1,5 +1,5 @@
 var options = {
-    manager: require("../../../src/managers/purchasing/unit-payment-quantity-correction-manager"),
+    manager: require("../../../src/managers/purchasing/unit-payment-quantity-correction-note-manager"),
     model: require("dl-models").purchasing.UnitPaymentCorrectionNote,
     util: require("../../data-util/purchasing/unit-payment-quantity-correction-data-util"),
     validator: require("dl-models").validator.purchasing.unitPaymentCorrectionNote,
