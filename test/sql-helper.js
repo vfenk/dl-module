dfb3d44b19f68308af3248336ec5c265656f8975
Leslie Aula <cristiano.ulo@gmail.com>
2017-01-30 11:50:39 +0700

    update sql helper

--- test/sql-helper.js
+++ test/sql-helper.js
@@ -15,7 +15,6 @@ module.exports = {
         return new Promise((resolve, reject) => {
             sql.connect(config, function (err) {
                 resolve(new sql.Request());
-                
             });
         });
     }
