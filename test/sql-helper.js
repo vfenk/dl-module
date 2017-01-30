3ec14f42501a8361733b2f9d10a8731c3576fd78
Leslie Aula <cristiano.ulo@gmail.com>
2017-01-30 11:49:28 +0700

    update sql helper

---
+++ test/sql-helper.js
@@ -0,0 +1,22 @@
+module.exports = {
+    getConnection: function () {
+        var sql = require("mssql");
+        var config = {
+            server: process.env.SQL_SERVER,
+            database: process.env.SQL_DATABASE,
+            user: process.env.SQL_USERNAME,
+            password: process.env.SQL_PASSWORD,
+            options: {
+                encrypt: true
+            },
+            connectionTimeout: 120 * 60 * 1000,
+            requestTimeout: 1000000
+        };
+        return new Promise((resolve, reject) => {
+            sql.connect(config, function (err) {
+                resolve(new sql.Request());
+                
+            });
+        });
+    }
+};
