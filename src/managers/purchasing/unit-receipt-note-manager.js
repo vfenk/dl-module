f17d0e9981e87cb0ccd8f572f09c6512602bb5b5
indriHutabalian <indri.hutabalian@moonlay.com>
2017-02-07 17:36:14 +0700

    fix validation of date

--- src/managers/purchasing/unit-receipt-note-manager.js
+++ src/managers/purchasing/unit-receipt-note-manager.js
@@ -90,8 +90,9 @@ module.exports = class UnitReceiptNoteManager extends BaseManager {
                     if (valid.deliveryOrder) {
                         if (!valid.deliveryOrder._id)
                             errors["deliveryOrder"] = i18n.__("UnitReceiptNote.deliveryOrder.isRequired:%s is required", i18n.__("UnitReceiptNote.deliveryOrder._:Delivery Order No")); //"No. surat jalan tidak boleh kosong";
-                        var doDate = valid.deliveryOrder.date;
-                        if (valid.date < doDate)
+                        var doDate = new Date(valid.deliveryOrder.date);
+                        var validDate = new Date(valid.date);
+                        if (validDate < doDate)
                             errors["date"] = i18n.__("DeliveryOrder.date.isGreater:%s is greater than delivery order date", i18n.__("DeliveryOrder.date._:Date"));//"Tanggal surat jalan tidak boleh lebih besar dari tanggal hari ini";
                     }
                     else if (!valid.deliveryOrder)
