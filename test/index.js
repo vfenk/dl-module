df29ed2351c7343fad9967c3fb30d74037c0e6c7
achensetyawati <achenzetya@gmail.com>
2017-02-07 12:32:04 +0700

    test

--- test/index.js
+++ test/index.js
@@ -61,7 +61,7 @@ describe('#dl-module', function (done) {
     test('@PURCHASING/UNIT PAYMENT PRICE CORRECTION', './purchasing/unit-payment-price-correction-note');
     test('@PURCHASING/UNIT PAYMENT QUANTITY CORRECTION', './purchasing/unit-payment-quantity-correction-note');
     
-    test('@purchasing/purchase-order/report', './purchasing/purchase-order/report/report');		
+    //test('@purchasing/purchase-order/report', './purchasing/purchase-order/report/report');		
     //Production
 
     // test('@production/winding-quality-sampling-manager', './production/spinning/winding/winding-quality-sampling-manager-test');
