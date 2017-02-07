6f8b7a69a0492e77431d98ba81042ebb6cd9d752
Leslie Aula <cristiano.ulo@gmail.com>
2017-02-07 14:21:54 +0700

    update index

--- test/index.js
+++ test/index.js
@@ -44,7 +44,7 @@ describe('#dl-module', function (done) {
     test('@MASTER/INSTRUCTION', './master/instruction');
     test('@MASTER/MONITORING-EVENT-TYPE', './master/monitoring-event-type');
     test('@MASTER/STEP', './master/step');
-    test('@MASTER/MACHINE-TYPE', './master/machine-type');
+    // test('@MASTER/MACHINE-TYPE', './master/machine-type');
     test('@MASTER/MACHINE-SPESIFICATION-STANDARD', './master/machine-spesification-standard');
     test('@MASTER/MATERIAL-CONSTRUCTION', './master/material-construction');
     test('@MASTER/YARN-MATERIAL', './master/yarn-material');
