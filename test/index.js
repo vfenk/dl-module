5f0216c21c776f7a036241e6eb15dbf3eafb7fd7
Leslie Aula <cristiano.ulo@gmail.com>
2017-02-16 14:10:36 +0700

    index update

--- test/index.js
+++ test/index.js
@@ -16,6 +16,7 @@ describe('#dl-module', function (done) {
     test('@ETL/DIM-CATEGORY', './etl/dim-category');
     // test('@ETL/DIM-DIVISION', './etl/dim-division');
     test('@ETL/DIM-SUPPLIER', './etl/dim-supplier');
+    test('@ETL/DIM-MACHINE', './etl/dim-machine');
     // test('@ETL/DIM-UNIT', './etl/dim-unit');
     // test('@ETL/FACT-TOTAL-HUTANG', './etl/fact-total-hutang');
     test('@ETL/FACT-PURCHASING', './etl/fact-purchasing');    
