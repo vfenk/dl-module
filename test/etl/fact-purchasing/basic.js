939373f694f198ecf00e672cc6b818c39e7bac89
Leslie Aula <cristiano.ulo@gmail.com>
2017-02-16 16:26:17 +0700

    update etl

--- test/etl/fact-purchasing/basic.js
+++ test/etl/fact-purchasing/basic.js
@@ -1,6 +1,6 @@
 var helper = require("../../helper");
 // var Manager = require("../../../src/etl/fact-purchasing-etl-manager");
-var Manager = require("../../../src/etl/test");
+var Manager = require("../../../src/etl/fact-pembelian");
 var instanceManager = null;
 var should = require("should");
 var sqlHelper = require("../../sql-helper");
