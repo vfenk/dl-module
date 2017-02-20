19cffd75d3eebba1b9687b75a7b772e2874f5829
Leslie Aula <cristiano.ulo@gmail.com>
2017-02-20 18:49:22 +0700

    update etl

--- src/etl/fact-total-hutang-etl-manager.js
+++ src/etl/fact-total-hutang-etl-manager.js
@@ -192,16 +192,16 @@ module.exports = class FactTotalHutang extends BaseManager {
 
                         this.sql.multiple = true;
 
-                        var fs = require("fs");
-                        var path = "C:\\Users\\leslie.aula\\Desktop\\tttt.txt";
+                        // var fs = require("fs");
+                        // var path = "C:\\Users\\leslie.aula\\Desktop\\tttt.txt";
 
-                        fs.writeFile(path, sqlQuery, function (error) {
-                            if (error) {
-                                console.log("write error:  " + error.message);
-                            } else {
-                                console.log("Successful Write to " + path);
-                            }
-                        });
+                        // fs.writeFile(path, sqlQuery, function (error) {
+                        //     if (error) {
+                        //         console.log("write error:  " + error.message);
+                        //     } else {
+                        //         console.log("Successful Write to " + path);
+                        //     }
+                        // });
 
                         return Promise.all(command)
                             .then((results) => {
