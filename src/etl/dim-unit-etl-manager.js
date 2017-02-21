ac87f1bb667c28d44a01a6d11a9c67653bcdd3c5
Leslie Aula <cristiano.ulo@gmail.com>
2017-02-21 11:34:28 +0700

    update etl

--- src/etl/dim-unit-etl-manager.js
+++ src/etl/dim-unit-etl-manager.js
@@ -16,11 +16,42 @@ module.exports = class DimUnitEtlManager extends BaseManager {
         super(db, user);
         this.sql = sql;
         this.unitManager = new UnitManager(db, user);
+        this.migrationLog = this.db.collection("migration-log");
     }
+    
     run() {
+        var startedDate = new Date();
+        this.migrationLog.insert({
+            description: "Fact Pembelian from MongoDB to Azure DWH",
+            start: startedDate,
+        })
         return this.extract()
             .then((data) => this.transform(data))
-            .then((data) => this.load(data));
+            .then((data) => this.load(data))
+            .then((result) => {
+                var finishedDate = new Date();
+                var spentTime = moment(finishedDate).diff(moment(startedDate), "minutes");
+                var updateLog = {
+                    description: "Fact Pembelian from MongoDB to Azure DWH",
+                    start: startedDate,
+                    finish: finishedDate,
+                    executionTime: spentTime + " minutes",
+                    status: "Successful"
+                };
+                this.migrationLog.updateOne({ start: startedDate }, updateLog);
+            })
+            .catch((err) => {
+                var finishedDate = new Date();
+                var spentTime = moment(finishedDate).diff(moment(startedDate), "minutes");
+                var updateLog = {
+                    description: "Fact Pembelian from MongoDB to Azure DWH",
+                    start: startedDate,
+                    finish: finishedDate,
+                    executionTime: spentTime + " minutes",
+                    status: err
+                };
+                this.migrationLog.updateOne({ start: startedDate }, updateLog);
+            });
     }
 
     extract() {
@@ -42,32 +73,101 @@ module.exports = class DimUnitEtlManager extends BaseManager {
         return Promise.resolve([].concat.apply([], result));
     }
 
-    load(data) {
-        return this.sql.getConnection()
-            .then((request) => {
-
-                var sqlQuery = '';
-
-                var count = 1;
-                for (var item of data) {
-                    sqlQuery = sqlQuery.concat("insert into DL_Dim_Unit(ID_dim_unit, Kode_unit, Nama_Divisi, Nama_unit) values(" + count + ", '" + item.unitCode + "', '" + item.divisionName + "', '" + item.unitName + "'); ");
-
-                    count = count + 1;
+    insertQuery(sql, query) {
+        return new Promise((resolve, reject) => {
+            sql.query(query, function(err, result) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result);
                 }
-
-                request.multiple = true;
-
-                return request.query(sqlQuery)
-                    // return request.query('select count(*) from Dimunit')
-                    // return request.query('select top 1 * from Dimunit')
-                    .then((results) => {
-                        console.log(results);
-                        return Promise.resolve();
-                    })
             })
+        })
+    }
+
+    load(data) {
+        return new Promise((resolve, reject) => {
+            this.sql.startConnection()
+                .then(() => {
+
+                    var transaction = this.sql.transaction();
+
+                    transaction.begin((err) => {
+
+                        var request = this.sql.transactionRequest(transaction);
+
+                        var command = [];
+
+                        var sqlQuery = '';
+
+                        var count = 1;
+
+                        for (var item of data) {
+                            if (item) {
+                                var queryString = `insert into DL_Dim_Unit_Temp(ID_dim_unit, Kode_unit, Nama_Divisi, Nama_unit) values(${count}, '${item.unitCode}', '${item.divisionName}', '${item.unitName}');\n`;
+                                sqlQuery = sqlQuery.concat(queryString);
+                                if (count % 1000 == 0) {
+                                    command.push(this.insertQuery(request, sqlQuery));
+                                    sqlQuery = "";
+                                }
+                                console.log(`add data to query  : ${count}`);
+                                count++;
+                            }
+                        }
+
+
+                        if (sqlQuery !== "")
+
+                            command.push(this.insertQuery(request, `${sqlQuery}`));
+
+                        this.sql.multiple = true;
+
+                        return Promise.all(command)
+                            .then((results) => {
+                                request.execute("DL_UPSERT_DIM_UNIT").then((execResult) => {
+                                    request.execute("DL_INSERT_DIMTIME").then((execResult) => {
+                                        transaction.commit((err) => {
+                                            if (err)
+                                                reject(err);
+                                            else
+                                                resolve(results);
+                                        });
+                                    }).catch((error) => {
+                                        transaction.rollback((err) => {
+                                            console.log("rollback")
+                                            if (err)
+                                                reject(err)
+                                            else
+                                                reject(error);
+                                        });
+                                    })
+                                }).catch((error) => {
+                                    transaction.rollback((err) => {
+                                        console.log("rollback")
+                                        if (err)
+                                            reject(err)
+                                        else
+                                            reject(error);
+                                    });
+                                })
+                            })
+                            .catch((error) => {
+                                transaction.rollback((err) => {
+                                    console.log("rollback");
+                                    if (err)
+                                        reject(err)
+                                    else
+                                        reject(error);
+                                });
+                            });
+                    })
+                })
+                .catch((err) => {
+                    reject(err);
+                })
+        })
             .catch((err) => {
-                console.log(err);
-                return Promise.reject(err);
-            });
+                reject(err);
+            })
     }
 }
